f2af58ffa16991e9a29bcc08a2f54b2c3263adde
mcr45 <depr3604@protonmail.com>
2023-04-22 16:30:01 -0500

    created random recipe page with api

--- assets/js/random.js
+++ assets/js/random.js
@@ -1,27 +1,18 @@
-document.querySelector('#cercami').addEventListener('click', getFetch)
-//4e35b7b843f549b4b8ba014950f4dbc7 apiKey
-// per usarew apikey url = alla fine ?apiKey=4e35b7b843f549b4b8ba014950f4dbc7
-//https://api.spoonacular.com/food/products/search?query=${choice}&number=7&apiKey=4e35b7b843f549b4b8ba014950f4dbc7
+window.onload=getFetch
+
 function getFetch(){
-  const choice = document.querySelector('#groce').value
-  //console.log(choice)
-  const url = `https://api.spoonacular.com/food/products/search?query=${choice}&number=12&apiKey=4e35b7b843f549b4b8ba014950f4dbc7`
-  let arr
+ 
+  const url = `https://api.spoonacular.com/recipes/random?number=1&limitLicense=true&tags=vegetarian&apiKey=4e35b7b843f549b4b8ba014950f4dbc7`
+  
   
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-        arr=data.products
-        arr.forEach((e,i) => {
-        document.querySelector(`img-${i}`).src=e.image
-        document.querySelector(`.portamivia-${i}>h2`).innerHTML=e.title
-        document.querySelector(`.portamivia-${i}`).id=e.id
-        //posso mettere link alla pagina tipo generichtml parentesi id
-        //aggiungo event listener che setta localstorstorage variable che usero in generichtml o wathever
-        document.querySelector(`.portamivia-${i}`).addEventListener('click',getDetail)
-
-          
-        });
+        //console.log(data.recipes[0])
+       document.querySelector('#atene').src=data.recipes[0].image
+       document.querySelector('#titolo').innerHTML=data.recipes[0].title
+       document.querySelector('#somario').innerHTML=data.recipes[0].summary
+       document.querySelector('#istruzioni').innerHTML=data.recipes[0].instructions
         
       })
       .catch(err => {
@@ -29,10 +20,3 @@ function getFetch(){
       });
 }
 
-function getDetail(event){
-let ins=event.target.id
-let urs=`https://api.spoonacular.com/food/products/${ins}`
-localStorage.setItem=(`link`,urs)
-
-
-}
